3ffc9034d112147ff891ac96ef04e672c54117a0
Dania Tamim <dania.tamim@live.se>
2024-09-30 12:47:15 +0200

    Add vehicle array to garage

--- Garage 1.0/Garage/Garage.cs
+++ Garage 1.0/Garage/Garage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Garage_1._0.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,14 @@ using System.Threading.Tasks;
 
 namespace Garage_1._0.Garage
 {
-    public class Garage
+    public class Garage<T> where T : Vehicle
     {
-        public Garage() { }
+        private readonly int capacity;
+        private Vehicle[] vehicles;
+        public Garage(int capacity)
+        {
+            this.capacity = capacity;
+            vehicles = new Vehicle[capacity];
+        }
     }
 }
