08a734c6ecd57f68d092045385fa91d4af8b7911
Dania Tamim <dania.tamim@live.se>
2024-09-30 10:12:38 +0200

    Add basic classes

---
+++ Garage 1.0/Entities/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_1._0.Entities
+{
+    public class Vehicle
+    {
+        private string regNum;
+        private string color;
+        private int numOfWheel;
+        public string RegNum { get => regNum; set => regNum = value; }
+        public string Color { get => color; set => color = value; }
+        public int NumOfWheel { get => numOfWheel; set => numOfWheel = value; }
+
+        public Vehicle(string regNum, string color , int numOfWheel)
+        {
+            RegNum = regNum;
+            Color = color;
+            NumOfWheel = numOfWheel;            
+        }
+    }
+}
